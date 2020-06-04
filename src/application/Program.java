c6198e7a90c3dabefea58d23b4fa2b4744a6df9e
David Wilker <davidwilker2510@gmail.com>
2020-06-04 03:46:33 -0300

    Criação class ChessMath e UI

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,14 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMath;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Position pos = new Position(3, 5);
-		Board board = new Board(8, 8);
+		ChessMath chessMath = new ChessMath();
+		UI.printBoard(chessMath.getPieces());
 		
 	}
 
