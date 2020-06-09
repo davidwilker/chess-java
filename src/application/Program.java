f2895c39a965200c88dd7f2f6d72503efc78094e
David Wilker <davidwilker2510@gmail.com>
2020-06-08 23:29:42 -0300

    Entrada de dados e movimento da peça

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,32 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMath;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+				
+		Scanner sc = new Scanner(System.in);
 		ChessMath chessMath = new ChessMath();
-		UI.printBoard(chessMath.getPieces());
+		
+		while(true) {
+			UI.printBoard(chessMath.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMath.performChessMove(source, target);
+		}
+		
 		
 	}
 
